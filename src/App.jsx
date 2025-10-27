972ac62b4513be4f4f9ad36ec1b5d1c54e1a4895
PYticPH <pyticph@gmail.com>
2025-10-27 12:28:44 -0400

    fix: Correct error due to invalid component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 
-import Header from './components/Header'
-import Info from './components/Info'
+import Header from './components/Info'
 import About from './components/About'
 import Interests from './components/Interests'
 import Footer from './components/Footer'
@@ -13,7 +12,6 @@ function App() {
   return (
     <>
       <Header />
-      <Info />
       <About />
       <Interests />
       <Footer />
